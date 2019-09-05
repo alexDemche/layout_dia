8e9218dc48e7ff082b23a489cd5a521b102bd6dc
alexDemche <alexeyydemchenko@gmail.com>
2019-09-05 09:41:48 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* Burger */
 const burger = document.getElementById('burger');
 const burgerBar = document.querySelector('.js-burger__bar');
 // const body = document.getElementById('body');
@@ -11,3 +12,21 @@ burger.addEventListener('click', () => {
   menu.classList.toggle('js-show-menu');
   // body.classList.toggle('js-no-scroll');
 });
+/* END Burger */
+
+/* Anchor smooth scroll */
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (const anchor of anchors) {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
+/* END Anchor smooth scroll */
